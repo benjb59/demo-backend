832e3d707ef73c51fc56404a479be8178833b9c8
Benjamin <benj.bour@gmail.com>
2021-02-11 15:06:38 +0100

    Mongodb

--- src/main/java/fr/home/backend/DemoBackendApplication.java
+++ src/main/java/fr/home/backend/DemoBackendApplication.java
@@ -2,8 +2,10 @@ package fr.home.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@EnableMongoRepositories
 public class DemoBackendApplication {
 
 	public static void main(String[] args) {
