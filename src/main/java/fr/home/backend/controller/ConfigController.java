92d03b2e20de05b52550942f47be2d175c959f4e
Benjamin <benj.bour@gmail.com>
2021-02-11 21:55:17 +0100

    Update conf

---
+++ src/main/java/fr/home/backend/controller/ConfigController.java
@@ -0,0 +1,26 @@
+package fr.home.backend.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class ConfigController {
+
+    @Autowired
+    private Environment env;
+
+    @GetMapping("/property")
+    public String getPropertyValue(@RequestParam("key") String key)
+    {
+        String returnValue = "No value";
+
+        String keyValue = env.getProperty(key);
+
+        if( keyValue!= null && !keyValue.isEmpty())
+        {
+            returnValue = keyValue;
+        }
+        return returnValue;
+    }
+}
