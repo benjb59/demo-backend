280b8af051bed842b4b508a96c6aaac9e2e0dd21
Benjamin Bour <benjamin.bour@insee.fr>
2021-03-10 15:15:07 +0100

    Update stress test

--- .k6/stress.js
+++ .k6/stress.js
@@ -1,5 +1,17 @@
 import http from 'k6/http';
 
+export let options = {
+  scenarios: {
+    questionnaires: {
+      executor: 'constant-arrival-rate',
+      rate: 50,
+      duration: '1m',
+      preAllocatedVUs: 50,
+      maxVUs: 100,
+    },
+  },
+};
+
 export default function () {
   const response = http.get("http://demo-backend-mongo/questionnaires");
 };
